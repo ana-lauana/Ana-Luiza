9de3ebc9b4c785cd63ad952d8fed9a8168762b64
Ana e Kelly <109979993+ana-lauana@users.noreply.github.com>
2024-08-01 11:34:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+    
+    const caixaPerguntas = document.querySelector(".caixa-perguntas");
+    
+    const caixaAlternativas = document.querySelector(".caixa-alternativas");
+    
+    const caixaResultado = document.querySelector(".caixa-resultado");
+    
+    const textoResultado = document.querySelector(".texto-resultado");
+    
+    const_perguntas = [
+    {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    ];
+
+                const perguntas = [
+                {
+                    enunciado: "Insira o enunciado da Pergunta 1",
+                    alternativas: [
+                    {
+                    texto: "Alternativa 1",
+                    afirmação: "Afirmação da alternativa 1"
+                    },
+                    {
+                    texto: "Alternativa 2",
+                    afirmação: "Afirmação da alternativa 2"
+                    }
+                    ]
+                 }
+                ]
+
+
+            
